b1221080eec73026e311749b34917e49d1c8efd7
Nadirsah <isayevnadirsah26@gmail.com>
2024-09-03 12:08:53 +0400

    start

---
+++ backindex.php
@@ -0,0 +1,24 @@
+<?php include('layouts/admin/head.php') ?>
+<div class="wrapper">
+  <!-- Sidebar -->
+  <?php include('layouts/admin/sidebar.php') ?>
+  <!-- End Sidebar -->
+
+  <div class="main-panel">
+    <?php include('layouts/admin/header.php') ?>
+
+    <div class="container">
+      <div class="page-inner">
+        Salam
+      </div>
+    </div>
+
+
+  </div>
+
+  <!-- Custom template | don't include it in your project! -->
+
+  <!-- End Custom template -->
+</div>
+<!--   Core JS Files   -->
+<?php include('layouts/admin/script.php') ?>
